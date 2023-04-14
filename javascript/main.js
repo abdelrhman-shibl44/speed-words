889c9602c6b263cc878b296a02437150b7bdf8c2
abdelrhman mahmoud shibl <abdulrhman.mahmoud44@gmail.com>
2023-04-14 13:05:11 +0200

    get data and make loop & save state in session storage

--- javascript/main.js
+++ javascript/main.js
@@ -1,186 +1,170 @@
 // make the array of words
-const words = [
-  "how",
-  "and",
-  "spend",
-  "about",
-  "communities",
-  "you",
-  "belong",
-  "because",
-  "enough",
-  "probably",
-  "take",
-  "tend",
-  "different",
-  "joined",
-  "play",
-  "forget",
-  "circle",
-  "many",
-  "first",
-  "woman",
-  "sklodowska",
-  "physics",
-  "prize",
-  "husband",
-  "Pirre",
-  "Nobel",
-  "thinking",
-  "people",
-  "music",
-  "video",
-  "studio",
-  "production",
-  "include",
-  "those",
-  "stage",
-  "musican",
-  "marine",
-  "windMills",
-  "Germany",
-  "indonesia",
-  "progrmming",
-];
+const getData = async () => {
+  const res = await fetch("../data/data.json");
+  const data = await res.json();
+  const words = data.map((item) => item.word);
+  return words;
+}
 
 // get the items
-TimeLeftSpan = document.querySelector(".timeLeft");
-TotalSpan = document.querySelector(".control .total");
-thecontainer = document.querySelector(".speedWordsGame");
-theInput = document.querySelector(".speedWordsGame input");
-thebutton = document.querySelector(".speedWordsGame button");
-thechoosenWord = document.querySelector(".speedWordsGame .theWord");
-theUpcomingWords = document.querySelector(".speedWordsGame .theUpcoming_words");
-theLeftTime = document.querySelector(".timeLeft");
-acheivedScore = document.querySelector(".speedWordsGame .got");
-secondsSpan = document.querySelector(".message span:last-child");
-theNormalvl = document.querySelector(".Normal");
-lvlSpan = document.querySelector(".message > span");
-arrowHolder = document.querySelector(".arrowHolder")
-
+const totalSpan   = document.querySelector(".control .total");
+const container  = document.querySelector(".speedWordsGame");
+const input  = document.querySelector(".speedWordsGame input");
+const button  = document.querySelector(".speedWordsGame button");
+const choosenWord = document.querySelector(".speedWordsGame .theWord");
+const upcomingWords  = document.querySelector(".speedWordsGame .theUpcoming_words");
+const leftTime = document.querySelector(".timeLeft");
+const score = document.querySelector(".speedWordsGame .got");
+const seconds = document.querySelector(".message span:last-child");
+const normalVl  = document.querySelector(".Normal");
+const levelSpan = document.querySelector(".message > span");
+const arrowHolder = document.querySelector(".arrowHolder");
+let state = sessionStorage.getItem("state") || "Normal"
+console.log(state)
 // Setting Levels
 const lvls = {
-  Easy: 3,
+  Easy: 6,
   Normal: 4,
   Hard: 2,
 };
 // Default Lvl
-let defaultLevelName = "Normal"; // here you can change level
+let defaultLevelName = state || "Normal"; // here you can change level
 let defaultLevelSeconds = lvls[defaultLevelName];
 
 document.querySelector(".decorated li:first-child").onclick = function () {
-  theNormalvl.innerText = "Normal";
-  lvlSpan.innerText = "Normal";
-  secondsSpan.innerText = 4;
-  TimeLeftSpan.innerText = 4;
+  sessionStorage.setItem("state", "Normal")
+  normalVl.innerText = "Normal"
+  levelSpan.innerText ="Normal"
+  seconds.innerText = 4;
+  leftTime.innerText = 4;
   removeOpen();
 };
 document.querySelector(".decorated li:nth-child(2)").onclick = function () {
-  theNormalvl.innerText = "Easy";
-  lvlSpan.innerText = "Easy";
-  secondsSpan.innerText = 3;
-  TimeLeftSpan.innerText = 3;
+  sessionStorage.setItem("state", "Easy")
+  normalVl.innerText = "Easy"
+  levelSpan.innerText ="Easy" 
+  seconds.innerText = 6;
+  leftTime.innerText = 6;
   removeOpen();
 };
 
 document.querySelector(".decorated li:last-child").onclick = function () {
-  theNormalvl.innerText = "Hard";
-  lvlSpan.innerText = "Hard";
-  secondsSpan.innerText = 2;
-  TimeLeftSpan.innerText = 2;
+  sessionStorage.setItem("state","Hard")
+  normalVl.innerText = "Hard"
+  levelSpan.innerText = "Hard"
+  seconds.innerText = 2;
+  leftTime.innerText = 2;
   removeOpen();
 };
+
 
 function removeOpen() {
   document.querySelector(".decorated").classList.remove("open");
 }
 // addContent to page
-lvlSpan.innerText = defaultLevelName;
-secondsSpan.innerText = defaultLevelSeconds;
-TimeLeftSpan.innerText = defaultLevelSeconds;
-lenghtOfWords = words.length;
-TotalSpan.innerText = lenghtOfWords;
+levelSpan.innerText = state
+normalVl.innerText = state
+seconds.innerText = defaultLevelSeconds;
+leftTime.innerText = defaultLevelSeconds;
 // disablepasteEvent
-theInput.onpaste = function () {
+input.onpaste = function () {
   return false;
 };
 // whenClick
-thebutton.onclick = function () {
+button.onclick = function () {
   setTimeout(function () {
-    TimeLeftSpan.innerText = 6;
-  }, 0);
+    leftTime.innerText = 6;
+  }, 100);
   this.remove();
-  theInput.focus();
+  input.focus();
   getRandomWord();
 };
 // get random word
-function getRandomWord() {
-  randomWord = words[Math.floor(Math.random() * words.length)];
-  // get the indexOf the word
-  wordIndex = words.indexOf(randomWord);
-  // remove the word from the page by its index
+let words = []
+async function getRandomWord() {
+  words = await getData();
+  const lengthOfWords = words.length;
+  totalSpan.innerText = lengthOfWords;
+   randomWord = words[Math.floor(Math.random() * 50)];
+   // get the indexOf the word
+  const wordIndex = words.indexOf(randomWord);
+   // remove the word from the page by its index
   words.splice(wordIndex, 1);
   console.log(words);
-  // empty the upcoming words
-  theUpcomingWords.innerText = "";
-  // put the randomword to the page
-  span = document.createElement("span");
-  thechoosenWord.innerText = randomWord;
-  // createSpan and put it in the page
-  words.forEach((word) => {
-    span = document.createElement("span");
+   // empty the upcoming words
+  upcomingWords.innerText = "";
+   // put the randomword to the page
+  choosenWord.innerText = randomWord;
+   // createSpan and put it in the page
+  words.splice(0,50).forEach((word) => {
+    const span = document.createElement("span");
     span.innerText = word;
-    theUpcomingWords.appendChild(span);
+    upcomingWords.appendChild(span);
   });
   playwords();
 }
+// start interval
+let start = null
 
-acheivedScore.innerText = 0;
-acheivedScoreLocak = 1;
+score.innerText = 0;
+scoreLocak = 1;
 function playwords() {
-  if (lvlSpan.innerText === "Normal") {
-    TimeLeftSpan.innerText = 4;
+  leftTime.innerText = defaultLevelSeconds;
+  if (levelSpan.innerText === "Normal") {
+    leftTime.innerText = 4;
   }
-  if (lvlSpan.innerText === "Easy") {
-    TimeLeftSpan.innerText = 3;
+  if (levelSpan.innerText === "Easy") {
+    leftTime.innerText = 6;
   }
-  if (lvlSpan.innerText === "Hard") {
-    TimeLeftSpan.innerText = 2;
+  if (levelSpan.innerText === "Hard") {
+    leftTime.innerText = 2;
   }
-  TimeLeftSpan.innerText = defaultLevelSeconds;
 
   // setting time up
-  let start = setInterval(function () {
-    theLeftTime.innerText--;
-    if (theLeftTime.innerText === "0") {
+  start = setInterval( async function () {
+    leftTime.innerText--;
+    if (leftTime.innerText === "0") {
       clearInterval(start);
       // chek if two words are the same
-      if (theInput.value.toLowerCase() === randomWord.toLowerCase()) {
-        theInput.value = "";
-        acheivedScore.innerText++;
-        localStorage.setItem("date", new Date().toString());
-        window.localStorage.setItem("score", acheivedScoreLocak++);
-        if (words.length > 0) {
-          getRandomWord();
-        } else {
-          div = document.createElement("div");
-          div.innerHTML = "Congratulations </br> All Answers Is Right";
-          div.className = "good";
-          theUpcomingWords.appendChild(div);
-        }
-      } else {
-        span = document.createElement("span");
-        restartBtn = document.createElement("button");
-        restartBtn.appendChild(document.createTextNode("Restart"));
-        restartBtn.className = "restartBtn";
-        span.appendChild(document.createTextNode("GameOver"));
-        span.appendChild(restartBtn)
-        span.className = "GameOver";
-        document.querySelector("body").appendChild(span);
-        restartBtn.onclick = () => location.reload()
-      }
+        checkWords()
     }
   }, 1000);
+}
+
+// when click enter get the next word 
+input.addEventListener("keydown", (e) => {
+  if(e.key === "Enter"){
+    checkWords()
+    clearInterval(start);
+    e.target.value = "";
+  }
+});
+// check words 
+  const checkWords = () => {
+    if (input.value.toLowerCase() === randomWord.toLowerCase()) {
+      input.value = "";
+      score.innerText++;
+      localStorage.setItem("date", new Date().toString());
+      window.localStorage.setItem("score", scoreLocak++);
+      if (words.length > 0) {
+        getRandomWord();
+    } else {
+      div = document.createElement("div");
+      div.innerHTML = "Congratulations </br> All Answers Is Right";
+      div.className = "success";
+      upcomingWords.appendChild(div);
+    }
+  } else {
+    span = document.createElement("span");
+    restartBtn = document.createElement("button");
+    restartBtn.appendChild(document.createTextNode("Restart"));
+    restartBtn.className = "restartBtn";
+    span.appendChild(document.createTextNode("Wrong word"));
+    span.appendChild(restartBtn);
+    span.className = "GameOver";
+    document.querySelector("body").appendChild(span);
+    restartBtn.onclick = () => location.reload();
+  }
 }
 // when click on select
 arrowHolder.addEventListener("click", function () {
